b1634902a03dddb21d6186bead3f0963cebf8ffb
Nick Tom Thomas <n.thomas@Nicks-MBP-2.ht.home>
2023-05-10 08:18:18 -0400

    progress so far

--- marketplace-rental/rocket/src/main.rs
+++ marketplace-rental/rocket/src/main.rs
@@ -1,5 +1,14 @@
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+
+use rocket::http::Status;
+use rocket_contrib::databases::database;
+use rocket_contrib::json::JsonValue;
+
+mod models;
+mod schema;
 
 #[get("/")]
 fn index() -> &'static str {
